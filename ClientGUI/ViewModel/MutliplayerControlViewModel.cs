b40bec7a21c595776ff94e3acf6c6d1feb219376
benwex93 <benwex93@hotmail.com>
2016-05-10 20:14:08 +0300

    added player and multiplayer control

---
+++ ClientGUI/ViewModel/MutliplayerControlViewModel.cs
@@ -0,0 +1,47 @@
+﻿using ClientGui.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace ClientGui.ViewModel
+{
+    public class MutliplayerControlViewModel
+    {
+        MultiplayerControlModel model;
+        public MutliplayerControlViewModel()
+        {
+            model = new MultiplayerControlModel();
+            mainMenuCommand = new ButtonICommand(model.SinglePlayerOption);
+            getHintCommand = new ButtonICommand(model.getHintOnMaze);
+        }
+        //  public event PropertyChangedEventHandler PropertyChanged;
+        private ButtonICommand mainMenuCommand;
+        private ButtonICommand getHintCommand;
+
+        //  public SolidColorBrush backgroundColor { get; set; } = new SolidColorBrush(Color.FromRgb(0, 0, 0));
+        //public Visibility menuSelectionVisibility3 { get; set; } = Visibility.Hidden;
+
+        public ICommand goToMainMenu
+        {
+            get
+            {
+                return mainMenuCommand;
+            }
+        }
+        public ICommand getHint
+        {
+            get
+            {
+                return getHintCommand;
+            }
+        }
+        /** public void MainMenuVM_PropertyChanged(PropertyChangedEventArgs e)
+         {
+             if (PropertyChanged != null)
+                 PropertyChanged(this, e);
+         }**/
+    }
+}
