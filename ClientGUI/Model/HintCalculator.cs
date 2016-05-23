edc7eed837800e553bc931e93836b967f2eb8b50
benwex93 <benwex93@hotmail.com>
2016-05-23 20:59:30 +0300

    benjys commit

---
+++ ClientGUI/Model/HintCalculator.cs
@@ -0,0 +1,37 @@
+﻿using ClientGui.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClientGui.Model
+{
+    public class HintCalculator
+    {
+        private PlayerViewModel player, hintBox1, hintBox2, hintBox3;
+        public HintCalculator(PlayerViewModel player, PlayerViewModel  hintBox1, PlayerViewModel hintBox2, 
+            PlayerViewModel hintBox3)
+        {
+            this.player = player;
+            this.hintBox1 = hintBox1;
+            this.hintBox2 = hintBox2;
+            this.hintBox3 = hintBox3;
+        }
+        public PlayerViewModel GetHintBox1()
+        {
+            //BFStoBox(hi)
+            return hintBox1;
+        }
+        public PlayerViewModel GetHintBox2()
+        {
+            //BFStoBox(hi)
+            return hintBox2;
+        }
+        public PlayerViewModel GetHintBox3()
+        {
+            //BFStoBox(hi)
+            return hintBox3;
+        }
+    }
+}
---
+++ ClientGUI/View/VictoryScreen.xaml.cs
@@ -0,0 +1,34 @@
+﻿using ClientGui.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ClientGui.View
+{
+    /// <summary>
+    /// Interaction logic for VictoryScreen.xaml
+    /// </summary>
+    public partial class VictoryScreen : Window
+    {
+        public VictoryScreen()
+        {
+            InitializeComponent();
+        }
+
+        private void OKbutton_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+            AppModel.SwitchCurrentView(new MainMenuControl());
+        }
+    }
+}
